173b89ce9f9d7723deb03cd816f79b88ee651caf
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-11 09:37:24 -0300

    Instala e configura o pacote Express

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // app.js
-const http = require("http");
+// const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Olá Mundo!\\n");
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Olá Mundo!\\n");
+// });
+
+// server.listen(3000, "127.0.0.1", () => {
+//   console.log("Servidor rodando em <http://127.0.0.1:3000/>");
+// });
+
+app.get("/", (req, res) => {
+  res.send("Olá, mundo!");
 });
 
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Servidor rodando em <http://127.0.0.1:3000/>");
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
